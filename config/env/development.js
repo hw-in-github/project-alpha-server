8131a3e776d79adf2499f89d8303b43c09b6b380
NSWhoohoo <nswhoohoo@gmail.com>
2019-05-26 04:01:51 +0800

    change

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 export default {
   db: 'mongodb://localhost/project-demo',
-  port: 3000,
-  base: 'http://localhost:3000',
+  port: 5050,
+  base: 'http://localhost:5050',
   qiniu: {
     bucket: 'yourbucket',
     video: 'http://yourvideourl.xxx.com/',
